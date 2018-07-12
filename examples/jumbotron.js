fe8b9b8f4b935ecffc72b952d0767d262c0cf622
mjv761 <michael.vanderkamp@usask.ca>
2018-07-12 13:24:23 -0600

    Initial cleanup of jumbotron example.

--- examples/jumbotron.js
+++ examples/jumbotron.js
@@ -1,48 +1,72 @@
-var WAMS = require("../src/server");   // Includes the WAMS API
+/*
+ * This example is intended to demonstrate having multiple users move their
+ *  view around in a shared space.
+ */
+const WAMS = require('../src/server');
 
-var workspace_one = new WAMS.WorkSpace(9000);    // Starts server listening on port 3000, takes in optional parameter settings = {debug: true/false, BGColor: "#000000-FFFFFF"}
-    workspace_one.setBoundaries(4000, 4000);     // Set global boundaries for clients
-    workspace_one.setClientLimit(4);             // Set Client Limit
-
-// Creating and Adding Workspace objects. WSObject("Image.png", x, y, w, h)
-//var monaLisa = new WAMS.WSObject("monaLisa.png", 0, 0, workspace_one.width, workspace_one.height);
-//    monaLisa.setType("mona");   // Set your custom Object type
-var monaLisa = new WAMS.WSObject(0,0,workspace_one.width,workspace_one.height,"mona", {"imgsrc":"monaLisa.png"});
-workspace_one.addWSObject(monaLisa);
-
-// Handle Drag, takes in the target that was dragged on and who caused the drag event
-var handleDrag = function(target, client, x, y, dx, dy){
-    if(target.type == "client/background"){ // "client/background" is the type if your drag isen't on any objects
-        target.move(dx, dy);
+const workspace = new WAMS.WorkSpace(9000);
+workspace.setBoundaries(4000, 4000);
+workspace.setClientLimit(4);
+workspace.addWSObject(new WAMS.WSObject(
+    0,
+    0,
+    workspace.width,
+    workspace.height,
+    'mona',
+    {
+        imgsrc: 'monaLisa.png'
     }
-    else if(target.type == "mona"){ // We can check if target was our custom type, still just want to move the client anyway
+));
+
+// Takes in the target that was dragged on and who caused the drag event
+const handleDrag = function(target, client, x, y, dx, dy) {
+    // 'view/background' is the type if your drag isn't on any objects
+    if (target.type === 'view/background') { 
+        target.move(dx, dy);
+    } else if (target.type === 'mona') { 
+        // We can check if target was our custom type,
+        //  still just want to move the client anyway
         client.move(dx, dy);
     }
 }
 
-// Example Layout function that takes in the newly added client and which workspace they joined
+// Example Layout function that takes in the newly added client and which 
+//  workspace they joined.
 // Lays out users in a decending staircase pattern
-var handleLayout = function(ws, client){
-    var otherUsers = ws.users;   // How you get the users of the project
-    
-    if(otherUsers.length == 0){ // First user don't move
-        // Do Nothing
-    }
-    else{
-        if(otherUsers.length % 2 == 0){ // Decide where to put the user based on parity of the number of other users            
-            client.moveToXY(otherUsers[otherUsers.length-1].right() - 10, otherUsers[otherUsers.length-1].top());
+const handleLayout = (function makeLayoutHandler() {
+    function getMove(num_users, prev_user) {
+        if (num_users % 2 === 0) { 
+            return {
+                x: prev_user.right() - 10;
+                y: prev_user.top();
+            };
         }
-        else{
-            client.moveToXY(otherUsers[otherUsers.length-1].left(), otherUsers[otherUsers.length-1].bottom() - 10);
-        }    
+        return {
+            x: prev_user.left();
+            y: prev_user.bottom() - 10;
+        };
     }
-}
+
+    function handleLayout(workspace, client) {
+        const otherUsers = workspace.users;
+        const num_users = otherUsers.length;
+        const prev_user = otherUsers[num_users - 1];
+        
+        if (num_users > 0) {
+            const move = getMove(num_users, prev_user);
+            client.moveToXY(move.x, move.y);
+        }
+    }
+
+    return handleLayout;
+})();
 
 // Handle Scale, uses the built in viewspace method rescale
-var handleScale = function(vs, newScale){
+const handleScale = function(vs, newScale) {
     vs.rescale(newScale);
 }
 
-workspace_one.attachDragHandler(handleDrag);
-workspace_one.attachLayoutHandler(handleLayout);
-workspace_one.attachScaleHandler(handleScale);
+workspace.attachDragHandler(handleDrag);
+workspace.attachLayoutHandler(handleLayout);
+workspace.attachScaleHandler(handleScale);
+
