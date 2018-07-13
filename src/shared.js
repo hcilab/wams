4a8e091cf7cfcc5defdb29f69f6f2e1440aa382b
mjv761 <michael.vanderkamp@usask.ca>
2018-07-13 16:44:25 -0600

    Start using 'jest' test library, test shared.js

--- src/shared.js
+++ src/shared.js
@@ -11,105 +11,163 @@
  */
 
 /*
- * This method on the Object class will set an already-existing property on an
- *  object to be immutable. In other words, it will configure it as such:
- *
- *      configurable: false
- *      writable: false
- *
- * It will have no effect on non-configurable properties, and will turn an
- *  accessor descriptor into a data descriptor. (I.e. if the property is
- *  defined with getters and setters, they will be lost). 
- *
- * It will have no effect on properties that do not exist directly on the
- *  Object (properties further up the prototype chain are not affected).
- *
- * It will affect both enumerable and non-enumerable properties.
- *
- * This method is intended for use when the only reason for a call to
- *  Object.defineProperty() was to make the property immutable.
+ * This object stores a set of core utilities for use by both the client and
+ *  the server.
  */
-if (!Object.makeOwnPropertyImmutable) {
-    Object.makeOwnPropertyImmutable = function(obj, prop) {
+const WamsShared = (function defineSharedWamsModule() {
+    /*
+     * This method will set an already-existing property on an object to be 
+     *  immutable. In other words, it will configure it as such:
+     *
+     *      configurable: false
+     *      writable: false
+     *
+     * It will have no effect on non-configurable properties, and will turn an 
+     *  accessor descriptor  a data descriptor. (I.e. if the property is 
+     *  defined with getters and setters, they will be lost).  
+     *
+     * It will have no effect on properties that do not exist directly on the
+     *  Object (properties further up the prototype chain are not affected).
+     *
+     * It will affect both enumerable and non-enumerable properties.
+     *
+     * This method is intended for use when the only reason for a call to
+     *  Object.defineProperty() was to make the property immutable.
+     *
+     * Returns the modified object.
+     */
+    function makeOwnPropertyImmutable(obj, prop) {
         const desc = Object.getOwnPropertyDescriptor(obj, prop);
         if (desc && desc.configurable) {
             Object.defineProperty(obj, prop, {
                 configurable: false,
                 writable: false
             });
-        }       
+        }
+        return obj;
     }
-} else {
-    console.warn(
-        'Object.makePropertyImmutable method collision.\n' +
-        'Program may behave unexpectedly.'
-    );
-}
 
-/*
- * I wrote this generator class to make ID generation more controlled.
- * The class has access to a private (local lexical scope) generator function
- *  and Symbol for generators, and exposes a stamp() method that stamps an
- *  immutable ID onto the given object. 
- *
- * stamp(object [,id]):
- *  This method takes one or two arguments:
- *  object:     The object to stamp with an id.
- *  id:         [optional]
- *              If provided, will stamp the given ID onto the object.
- *              If not provided, the stamper will generate an ID to use.
- *
- *  WARNING:    IDs from a stamper are not guaranteed to be unique if it is
- *              ever explicitly provided an ID with which to stamp an object.
- *
- * For example:
- *  
- *      const stamper = new IDStamper();
- *      const obj = {};
- *      stamper.stamp(obj);
- *      console.log(obj.id); // an integer unique to IDs stamped by stamper.
- *      obj.id = 2;          // has no effect.
- *      delete obj.id;       // false
- *
- *      const danger = {};
- *      stamper.stamp(danger, obj.id);  // Will work. 'danger' and 'obj' are
- *                                      // now both using the same ID.
- */
-const IDStamper = (function defineIDStamper() {
-    function* id_gen() {
-        function willNotOverflow(x) {
-            return x + 1 > x;
+    /*
+     * Returns a new object, with all the own properties of 'defaults' having
+     *  values from 'data', if found, otherwise with values from 'defaults'.
+     */
+    function initialize(defaults = {}, data = {}) {
+        const rv = {};
+        Object.keys(defaults).forEach( k => {
+            rv[k] = data.hasOwnProperty(k) ? data[k] : defaults[k];
+        });
+        return rv;
+    }
+
+    /*
+     * I wrote this generator class to make ID generation more controlled.
+     * The class has access to a private (local lexical scope) generator 
+     *  function and Symbol for generators, and exposes a stamp() method that 
+     *  stamps an immutable ID onto the given object. 
+     *
+     * stamp(object [,id]):
+     *  This method takes one or two arguments:
+     *  object:     The object to stamp with an id.
+     *  id:         [optional]
+     *              If provided, will stamp the given ID onto the object.
+     *              If not provided, the stamper will generate an ID to use.
+     *
+     *  WARNING:    IDs from a stamper are not guaranteed to be unique if it is
+     *              ever explicitly provided an ID with which to stamp an 
+     *              object.
+     *
+     * For example:
+     *  
+     *      const stamper = new IDStamper();
+     *      const obj = {};
+     *      stamper.stamp(obj);
+     *      console.log(obj.id); // an integer unique to IDs stamped by stamper
+     *      obj.id = 2;          // has no effect.
+     *      delete obj.id;       // false
+     *
+     *      const danger = {};
+     *      stamper.stamp(danger, obj.id);  // Will work. 'danger' & 'obj' are
+     *                                      // now both using the same ID.
+     */
+    const IDStamper = (function defineIDStamper() {
+        function* id_gen() {
+            function willNotOverflow(x) {
+                return x + 1 > x;
+            }
+
+            let next_id = 0;
+            while (willNotOverflow(next_id)) yield ++next_id;
+        }
+        const gen = Symbol();
+
+        class IDStamper {
+            constructor() {
+                this[gen] = id_gen();
+            }
+
+            stamp(obj, id) {
+                Object.defineProperty(obj, 'id', {
+                    value: id === undefined ? this[gen].next().value : id,
+                    configurable: false,
+                    enumerable: true,
+                    writable: false
+                });
+                return obj.id;
+            }
         }
 
-        let next_id = 0;
-        while (willNotOverflow(next_id)) yield ++next_id;
-    }
-    const gen = Symbol('generator');
+        return IDStamper;
+    })();
 
-    class IDStamper {
-        constructor() {
-            this[gen] = id_gen();
-        }
+    /*
+     * The IDStamper can be outside the factory, because it only ever does
+     * copy stamping, it never generates its own stamps.
+     */
+    const stamper = new IDStamper();
 
-        stamp(obj, id) {
-            Object.defineProperty(obj, 'id', {
-                value: id === undefined ? this[gen].next().value : id,
-                configurable: false,
+    /*
+     * This factory can generate the basic classes that need to communicate
+     *  property values between the client and server.
+     */
+    function reporterClassFactory(_coreProperties) {
+        const defaults = {};
+        _coreProperties.forEach( p => {
+            Object.defineProperty(defaults, p, {
+                value: null,
+                writable: false,
                 enumerable: true,
-                writable: false
+                configurable: false
             });
+        });
+
+        class Reporter {
+            constructor(data) {
+                return this.assign(initialize(defaults, data));
+            }
+
+            assign(data) {
+                _coreProperties.forEach( p => {
+                    if (data.hasOwnProperty(p)) this[p] = data[p] 
+                });
+                return this;
+            }
+
+            retrieve() {
+                const data = {};
+                _coreProperties.forEach( p => data[p] = this[p] );
+                if (this.hasOwnProperty('id')) stamper.stamp(data, this.id);
+                return data; 
+            }
         }
+
+        return Reporter;
     }
 
-    return IDStamper;
-})();
-
-/*
- * This ViewSpace class provides a common interface between the client and the
- *  server by which the ViewSpaces can interact safely.
- */
-const ViewSpace = (function defineViewSpace() {
-    const _coreProperties = [
+    /*
+     * This ViewSpace class provides a common interface between the client and 
+     * the server by which the ViewSpaces can interact safely.
+     */
+    const ViewSpace = reporterClassFactory([
         'x',
         'y',
         'width',
@@ -118,33 +176,13 @@ const ViewSpace = (function defineViewSpace() {
         'effectiveHeight',
         'scale',
         'rotation',
-    ];
-    const stamper = new IDStamper();
+    ]);
 
-    class ViewSpace {
-        constructor() {
-            _coreProperties.forEach( p => this[p] = 0 );
-            this.scale = 1;
-        }
-
-        assign(data) {
-            _coreProperties.forEach( p => this[p] = data[p] );
-            return this;
-        }
-
-        retrieve() {
-            const data = {};
-            _coreProperties.forEach( p => data[p] = this[p] );
-            if (this.hasOwnProperty('id')) stamper.stamp(data, this.id);
-            return data; 
-        }
-    }
-
-    return ViewSpace;
-})();
-
-const WSObject = (function defineWSObject() {
-    const _coreProperties = [
+    /*
+     * This WSObject class provides a common interface between the client and 
+     * the server by which the WSObjects can interact safely.
+     */
+    const WSObject = reporterClassFactory([
         'x',
         'y',
         'width',
@@ -152,39 +190,29 @@ const WSObject = (function defineWSObject() {
         'imgsrc',
         'draw',
         'drawStart',
-    ];
-    const stamper = new IDStamper();
+    ]);
 
-    class WSObject {
-        constructor() {
-            _coreProperties.forEach( p => this[p] = null );
-        }
-
-        assign(data) {
-            _coreProperties.forEach( p => this[p] = data[p] );
-            return this;
-        }
-
-        retrieve() {
-            const data = {};
-            _coreProperties.forEach( p => data[p] = this[p] );
-            if (this.id) stamper.stamp(data, this.id);
-            return data;
-        }
-    }
-
-    return WSObject;
+    /*
+     * Package up the module and freeze it for delivery.
+     */
+    return Object.freeze({
+        makeOwnPropertyImmutable,
+        initialize,
+        IDStamper,
+        ViewSpace,
+        WSObject,
+    });
 })();
 
 /*
- * Conditionally export this file if we are running server-side with node.js
+ * Conditionally export this file if we are running server-side with node.js.
+ * Exporting in this manner lets the server side use whichever name for the 
+ * module that they prefer.
  *
  * XXX: Look into JS Modules. How do they work, and how widely supported are
  *      they?
  */
 if (typeof exports !== 'undefined') {
-    exports.IDStamper = IDStamper;
-    exports.ViewSpace = ViewSpace;
-    exports.WSObject = WSObject;
+    Object.keys(WamsShared).forEach( k => exports[k] = WamsShared[k] );
 }
 
---
+++ tests/shared.test.js
@@ -0,0 +1,393 @@
+/*
+ * This file is intented to be used for testing the shared module. It will test
+ * the module on the server side.
+ *
+ * TODO: Open up a server once tests are complete for testing code on the
+ *  client side.
+ */
+
+/*
+ * Routines to test:
+ *   + makeOwnPropertyImmutable,
+ *   + initialize,
+ *   + IDStamper,
+ *   + ViewSpace,
+ *   + WSObject,
+ */
+const shared = require('../src/shared.js');
+
+expect.extend({
+    toHaveImmutableProperty(received, argument) {
+        const descs = Object.getOwnPropertyDescriptor(received, argument);
+        const pass = Boolean(descs && !(descs.configurable || descs.writable));
+        const not = pass ? 'not ' : ''
+        return {
+            message: () =>
+                `expected ${received} ${not}to have immutable property '${argument}'`,
+            pass: pass,
+        };
+    },
+});
+
+/*
+ * This method will set an already-existing property on an object to be 
+ *  immutable. In other words, it will configure it as such:
+ *
+ *      configurable: false
+ *      writable: false
+ *
+ * It will have no effect on non-configurable properties, and will turn an 
+ *  accessor descriptor  a data descriptor. (I.e. if the property is 
+ *  defined with getters and setters, they will be lost).  
+ *
+ * It will have no effect on properties that do not exist directly on the
+ *  Object (properties further up the prototype chain are not affected).
+ *
+ * It will affect both enumerable and non-enumerable properties.
+ *
+ * This method is intended for use when the only reason for a call to
+ *  Object.defineProperty() was to make the property immutable.
+ *
+ * Returns the modified object.
+ *
+ *  function makeOwnPropertyImmutable(obj, prop) {
+ */
+describe('makeOwnPropertyImmutable', () => {
+    test('makes own enumerable, configurable property immutable', () => {
+        const x = {id: 1};
+        expect(x).not.toHaveImmutableProperty('id');
+        expect(shared.makeOwnPropertyImmutable(x, 'id')).toBe(x);
+        expect(delete x.id).toBe(false);
+        expect(
+            () => Object.defineProperty(x, 'id', {configurable: true})
+        ).toThrow();
+        expect(x).toHaveImmutableProperty('id');
+    });
+
+    test('does not affect non-configurable properties', () => {
+        const y = {};
+        Object.defineProperty(y, 'id', {
+            value:1, 
+            configurable: false, 
+            writable: true,
+            enumerable: true,
+        });
+        expect(y.id).toBe(1);
+
+        shared.makeOwnPropertyImmutable(y, 'id');
+        expect(y).not.toHaveImmutableProperty('id');
+
+        y.id = 2;
+        expect(y.id).toBe(2);
+    });
+
+    test('does not affect non-own properties', () => {
+        const p = {a: 1};
+        const q = Object.create(p);
+        expect(q.a).toBe(1);
+        expect(q).not.toHaveImmutableProperty('a');
+        expect(p).not.toHaveImmutableProperty('a');
+
+        shared.makeOwnPropertyImmutable(q, 'a');
+        expect(q).not.toHaveImmutableProperty('a');
+        expect(p).not.toHaveImmutableProperty('a');
+        p.a = 2;
+        expect(q.a).toBe(2);
+    });
+
+    test('does affect non-enumerable properties', () => {
+        const x = {};
+        Object.defineProperty(x, 'a', {
+            value: 1,
+            enumerable: false,
+            writable: true,
+            configurable: true
+        });
+        expect(x.a).toBe(1);
+
+        expect(x).not.toHaveImmutableProperty('a');
+        shared.makeOwnPropertyImmutable(x, 'a');
+        expect(x).toHaveImmutableProperty('a');
+    });
+});
+
+describe('initialize', () => {
+    test('does not throw exceptions on empty objects', () => {
+        expect(shared.initialize()).toEqual({});
+    });
+
+    test('returns empty if defaults is empty, regardless of data', () => {
+        expect(shared.initialize({},{})).toEqual({});
+        expect(shared.initialize({})).toEqual({});
+        expect(shared.initialize({},{a:1})).toEqual({});
+        expect(shared.initialize({},1)).toEqual({});
+    });
+
+    test('Uses defaults if data is empty.', () => {
+        expect(shared.initialize({a:1})).toEqual({a:1});
+        expect(shared.initialize({a:1},{})).toEqual({a:1});
+    });
+
+    test('Overrides default property if data has property with same name', () => {
+        expect(shared.initialize({a:1}, {a:2})).toEqual({a:2});
+    });
+});
+
+describe('IDStamper', () => {
+    const stamper = new shared.IDStamper();
+
+    describe('stamp(obj)', () => {
+        test('can stamp an immutable ID onto an object', () => {
+            const x = {};
+            expect(x).not.toHaveImmutableProperty('id');
+
+            const id = stamper.stamp(x);
+            expect(id).toBeGreaterThanOrEqual(0);
+            expect(x.id).toBeGreaterThanOrEqual(0);
+            expect(x.id).toBe(id);
+            expect(x).toHaveImmutableProperty('id');
+        });
+
+        test('cannot restamp an object', () => {
+            const x = {};
+            const id = stamper.stamp(x);
+            expect( () => stamper.stamp(x) ).toThrow();
+        });
+
+        test('does not reuse IDs', () => {
+            const x = {};
+            const y = {};
+            stamper.stamp(x);
+            stamper.stamp(y);
+            expect(x.id).not.toBe(y.id);
+        });
+    });
+
+    describe('stamp(obj, id)', () => {
+        test('will stamp an immutable user-provided ID', () => {
+            const x = {};
+            expect(x).not.toHaveImmutableProperty('id');
+
+            const id = 1;
+            const out = stamper.stamp(x, id);
+            expect(out).toBe(id);
+            expect(x).toHaveImmutableProperty('id');
+        });
+    });
+});
+
+describe('ViewSpace', () => {
+    const props = [
+        'x',
+        'y',
+        'width',
+        'height',
+        'effectiveWidth',
+        'effectiveHeight',
+        'scale',
+        'rotation',
+    ];
+
+    describe('constructor(data)', () => {
+        test('produces expected properties when no data provided', () => {
+            const vs = new shared.ViewSpace();
+            expect(Object.keys(vs)).toEqual(props);
+        });
+
+        test('uses provided data', () => {
+            const vs = new shared.ViewSpace({x:100, y:100, rotation: 90});
+            expect(Object.keys(vs)).toEqual(props);
+            expect(vs.x).toBe(100);
+            expect(vs.y).toBe(100);
+            expect(vs.rotation).toBe(90);
+            expect(vs.width).toBeNull();
+            expect(vs.scale).toBeNull();
+        });
+
+        test('does not use incorrect property names in data', () => {
+            const vs = new shared.ViewSpace({x: 100, y:100, z:100});
+            expect(Object.keys(vs)).toEqual(props);
+            expect(vs.x).toBe(100);
+            expect(vs.y).toBe(100);
+            expect(vs).not.toHaveProperty('z');
+        });
+    });
+
+    describe('assign(data)', () => {
+        const vs = new shared.ViewSpace();
+
+        test('throws exception if no data passed', () => {
+            expect(() => vs.assign()).toThrow(); 
+        });
+
+        test('assigns data', () => {
+            expect(vs.x).not.toBe(100);
+            vs.assign({x:100});
+            expect(vs.x).toBe(100);
+        });
+
+        test('reassigns data', () => {
+            expect(vs.x).toBe(100);
+            vs.assign({x:2});
+            expect(vs.x).toBe(2);
+        });
+
+        test('does not assign data that is not a core property', () => {
+            expect(vs).not.toHaveProperty('z');
+            vs.assign({z:100});
+            expect(vs).not.toHaveProperty('z');
+        });
+
+        test('only affects assigned properties', () => {
+            expect(vs.x).toBe(2);
+            expect(vs.y).not.toBe(1);
+            vs.assign({y:1});
+            expect(vs.x).toBe(2);
+            expect(vs.y).toBe(1);
+        });
+
+        test('is chainable', () => {
+            expect(vs.y).not.toBe(100);
+            expect(vs.width).not.toBe(50);
+            expect(
+                vs.assign({y:100}).assign({width:50})
+            ).toBe(vs);
+            expect(vs.y).toBe(100);
+            expect(vs.width).toBe(50);
+        });
+    });
+
+    describe('retrieve()', () => {
+        const vs = new shared.ViewSpace({x:100, y:50, width:200, height:300});
+        test('retrieves data', () => {
+            const data = vs.retrieve();
+            expect(Object.keys(data)).toEqual(props);
+            expect(data.x).toBe(100);
+            expect(data.y).toBe(50);
+            expect(data.width).toBe(200);
+            expect(data.height).toBe(300);
+            expect(data.scale).toBeNull();
+        });
+
+        test('does not retrieve an ID if none exists on the object', () => {
+            const data = vs.retrieve();
+            expect(data).not.toHaveProperty('id');
+        });
+
+        test('retrieves an immutable ID if one exists on the object', () => {
+            vs.id = 1;
+            const data = vs.retrieve();
+            expect(data).toHaveProperty('id');
+            expect(data.id).toBe(1);
+            expect(data).toHaveImmutableProperty('id');
+        });
+    });
+});
+
+describe('WSObject', () => {
+    const props = [
+        'x',
+        'y',
+        'width',
+        'height',
+        'imgsrc',
+        'draw',
+        'drawStart',
+    ];
+
+    describe('constructor(data)', () => {
+        test('produces expected properties when no data provided', () => {
+            const vs = new shared.WSObject();
+            expect(Object.keys(vs)).toEqual(props);
+        });
+
+        test('uses provided data', () => {
+            const vs = new shared.WSObject({x:100, y:100, imgsrc: 'a'});
+            expect(Object.keys(vs)).toEqual(props);
+            expect(vs.x).toBe(100);
+            expect(vs.y).toBe(100);
+            expect(vs.imgsrc).toBe('a');
+            expect(vs.width).toBeNull();
+            expect(vs.height).toBeNull();
+        });
+
+        test('does not use incorrect property names in data', () => {
+            const vs = new shared.WSObject({x: 100, y:100, z:100});
+            expect(Object.keys(vs)).toEqual(props);
+            expect(vs.x).toBe(100);
+            expect(vs.y).toBe(100);
+            expect(vs).not.toHaveProperty('z');
+        });
+    });
+
+    describe('assign(data)', () => {
+        const vs = new shared.WSObject();
+
+        test('throws exception if no data passed', () => {
+            expect(() => vs.assign()).toThrow(); 
+        });
+
+        test('assigns data', () => {
+            expect(vs.x).not.toBe(100);
+            vs.assign({x:100});
+            expect(vs.x).toBe(100);
+        });
+
+        test('reassigns data', () => {
+            expect(vs.x).toBe(100);
+            vs.assign({x:2});
+            expect(vs.x).toBe(2);
+        });
+
+        test('does not assign data that is not a core property', () => {
+            expect(vs).not.toHaveProperty('z');
+            vs.assign({z:100});
+            expect(vs).not.toHaveProperty('z');
+        });
+
+        test('only affects assigned properties', () => {
+            expect(vs.x).toBe(2);
+            expect(vs.y).not.toBe(1);
+            vs.assign({y:1});
+            expect(vs.x).toBe(2);
+            expect(vs.y).toBe(1);
+        });
+
+        test('is chainable', () => {
+            expect(vs.y).not.toBe(100);
+            expect(vs.width).not.toBe(50);
+            expect(
+                vs.assign({y:100}).assign({width:50})
+            ).toBe(vs);
+            expect(vs.y).toBe(100);
+            expect(vs.width).toBe(50);
+        });
+    });
+
+    describe('retrieve()', () => {
+        const vs = new shared.WSObject({x:100, y:50, width:200, height:300});
+        test('retrieves data', () => {
+            const data = vs.retrieve();
+            expect(Object.keys(data)).toEqual(props);
+            expect(data.x).toBe(100);
+            expect(data.y).toBe(50);
+            expect(data.width).toBe(200);
+            expect(data.height).toBe(300);
+            expect(data.imgsrc).toBeNull();
+        });
+
+        test('does not retrieve an ID if none exists on the object', () => {
+            const data = vs.retrieve();
+            expect(data).not.toHaveProperty('id');
+        });
+
+        test('retrieves an immutable ID if one exists on the object', () => {
+            vs.id = 1;
+            const data = vs.retrieve();
+            expect(data).toHaveProperty('id');
+            expect(data.id).toBe(1);
+            expect(data).toHaveImmutableProperty('id');
+        });
+    });
+});
+
