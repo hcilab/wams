4daee3adf5c729c88e42a73f95d97766d5431970
mjv761 <michael.vanderkamp@usask.ca>
2018-07-17 14:07:07 -0600

    Added test suite for ServerWSObject.

--- tests/server.test.js
+++ tests/server.test.js
@@ -114,5 +114,157 @@ describe('WorkSpace', () => {
       expect(ws.height).toBe(43);
     });
   });
-
 });
+
+describe('ServerWSObject', () => {
+  const DEFAULTS = Object.freeze({
+    x: 0,
+    y: 0,
+    width: 128,
+    height: 128,
+    type: 'view/background',
+    imgsrc: '',
+    drawCustom: '',
+    drawStart: '',
+  });
+
+  describe('constructor(settings)', () => {
+    test('Uses defaults if no arguments provided', () => {
+      let item;
+      expect(() => item = new ServerWSObject()).not.toThrow();
+      expect(item).toEqual(DEFAULTS);
+    });
+
+    test('Uses user-defined values, if provided', () => {
+      let item;
+      expect(() => {
+        item = new ServerWSObject({
+          y: 75,
+          type: 'joker',
+        });
+      }).not.toThrow();
+      expect(item.x).toBe(DEFAULTS.x);
+      expect(item.y).toBe(75);
+      expect(item.width).toBe(DEFAULTS.width);
+      expect(item.height).toBe(DEFAULTS.height);
+      expect(item.type).toBe('joker');
+      expect(item.imgsrc).toBe('');
+    });
+  });
+
+  describe('containsPoint(x,y)', () => {
+    const item = new ServerWSObject({
+      x: 50,
+      y: 50,
+      width: 100,
+      height: 100,
+    });
+
+    test('Accepts points completely inside square', () => {
+      expect(item.containsPoint(75,75)).toBe(true);
+      expect(item.containsPoint(51,51)).toBe(true);
+      expect(item.containsPoint(149,149)).toBe(true);
+    });
+
+    test('Rejects points completely outside the square', () => {
+      expect(item.containsPoint(0,0)).toBe(false);
+      expect(item.containsPoint(200,0)).toBe(false);
+      expect(item.containsPoint(0,200)).toBe(false);
+      expect(item.containsPoint(200,200)).toBe(false);
+    });
+
+    test('Accepts points on the border of the square', () => {
+      expect(item.containsPoint(50,50)).toBe(true);
+      expect(item.containsPoint(150,50)).toBe(true);
+      expect(item.containsPoint(50,150)).toBe(true);
+      expect(item.containsPoint(150,150)).toBe(true);
+    });
+
+    test('Rejects points just outside the border of the square', () => {
+      expect(item.containsPoint(49,49)).toBe(false);
+      expect(item.containsPoint(49,151)).toBe(false);
+      expect(item.containsPoint(151,49)).toBe(false);
+      expect(item.containsPoint(151,151)).toBe(false);
+    });
+    
+    test('Rejects when only one coordinate is valid', () => {
+      expect(item.containsPoint(25,75)).toBe(false);
+      expect(item.containsPoint(75,25)).toBe(false);
+    });
+
+    test('Rejects when one coordinate is barely outside the square', () => {
+      expect(item.containsPoint(49,50)).toBe(false);
+      expect(item.containsPoint(50,49)).toBe(false);
+      expect(item.containsPoint(150,151)).toBe(false);
+      expect(item.containsPoint(151,150)).toBe(false);
+    });
+  });
+
+  describe('moveToXY(x, y)', () => {
+    const item = new ServerWSObject({
+      x: 0,
+      y: 0,
+    });
+
+    test('Has no effect if parameters left out', () => {
+      expect(item.x).toBe(0);
+      expect(item.y).toBe(0);
+      expect(() => item.moveToXY()).not.toThrow();
+      expect(item.x).toBe(0);
+      expect(item.y).toBe(0);
+    });
+
+    test('Moves the object to the given coordinates.', () => {
+      expect(() => item.moveToXY(1000,9999)).not.toThrow();
+      expect(item.x).toBe(1000);
+      expect(item.y).toBe(9999);
+    });
+
+    test('Works with negative values', () => {
+      expect(() => item.moveToXY(-50, -1000)).not.toThrow();
+      expect(item.x).toBe(-50);
+      expect(item.y).toBe(-1000);
+    });
+
+    test('Does not affect other values', () => {
+      expect(() => item.moveToXY(DEFAULTS.x, DEFAULTS.y)).not.toThrow();
+      expect(item).toEqual(DEFAULTS);
+    });
+  });
+
+  describe('move(dx, dy)', () => {
+    const item = new ServerWSObject();
+
+    test('Has no effect if parameters left out', () => {
+      expect(item.x).toBe(0);
+      expect(item.y).toBe(0);
+      expect(() => item.move()).not.toThrow();
+      expect(item.x).toBe(0);
+      expect(item.y).toBe(0);
+    });
+
+    test('Moves the object by the given amount', () => {
+      expect(() => item.move(10,20)).not.toThrow();
+      expect(item.x).toBe(10);
+      expect(item.y).toBe(20);
+      expect(() => item.move(13,27)).not.toThrow();
+      expect(item.x).toBe(23);
+      expect(item.y).toBe(47);
+    });
+
+    test('Works with negative values', () => {
+      expect(() => item.move(-5,-8)).not.toThrow();
+      expect(item.x).toBe(18);
+      expect(item.y).toBe(39);
+      expect(() => item.move(-25,-48)).not.toThrow();
+      expect(item.x).toBe(-7);
+      expect(item.y).toBe(-9);
+    });
+
+    test('Has no effect on other values', () => {
+      expect(() => item.move(7,9)).not.toThrow();
+      expect(item).toEqual(DEFAULTS);
+    });
+  });
+});
+
