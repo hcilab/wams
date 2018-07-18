bd7ae0473e814f268173270dab8f8768ce071d6d
mjv761 <michael.vanderkamp@usask.ca>
2018-07-18 16:14:48 -0600

    Rearrange server tests.

--- tests/server.test.js
+++ tests/server.test.js
@@ -27,96 +27,6 @@ expect.extend({
   },
 });
 
-describe('WorkSpace', () => {
-  const DEFAULTS = Object.freeze({
-    debug: false,
-    color: '#aaaaaa',
-    bounds: {
-      x: 10000,
-      y: 10000,
-    },
-    clientLimit: 10,
-  });
-
-  describe('constructor(port, settings)', () => {
-    test('constructs correct type of object', () => {
-      expect(new WorkSpace()).toBeInstanceOf(WorkSpace);
-    });
-
-    test('Uses default port if none provided', () => {
-      expect(new WorkSpace().port).toBe(9000);
-    });
-
-    test('Uses user-defined port, if provided', () => {
-      expect(new WorkSpace(8080).port).toBe(8080);
-    });
-
-    test('Uses port as its ID', () => {
-      const ws1 = new WorkSpace();
-      expect(ws1.port).toBe(9000);
-      expect(ws1.id).toBe(9000);
-      
-      const ws2 = new WorkSpace(1264);
-      expect(ws2.id).toBe(1264);
-      expect(ws2.port).toBe(1264);
-    });
-
-    test('ID and port are immutable', () => {
-      const ws = new WorkSpace(8080);
-      expect(ws.id).toBe(8080);
-      expect(ws.port).toBe(8080);
-      expect(ws).toHaveImmutableProperty('id');
-      expect(ws).toHaveImmutableProperty('port');
-    });
-
-    test('Uses default settings if none provided', () => {
-      expect(new WorkSpace().settings).toEqual(DEFAULTS);
-    });
-
-    test('Uses user-defined settings, if provided', () => {
-      const custom = {
-        debug: true,
-        color: 'rgb(155,72, 84)',
-        bounds: {
-          x: 1080,
-          y: 1920,
-        },
-        clientLimit: 2,
-      };
-      expect(new WorkSpace(8080, custom).settings).toEqual(custom);
-
-      const ws = new WorkSpace(8080, {clientLimit: 7});
-      expect(ws.settings).not.toEqual(DEFAULTS);
-      expect(ws.settings.debug).toEqual(DEFAULTS.debug);
-      expect(ws.settings.color).toEqual(DEFAULTS.color);
-      expect(ws.settings.bounds).toEqual(DEFAULTS.bounds);
-      expect(ws.settings.clientLimit).toBe(7);
-    });
-  });
-
-  describe('getters and setters', () => {
-    const ws = new WorkSpace(8080, {bounds: {x:7,y:8}});
-
-    test('can get width', () => {
-      expect(ws.width).toBe(7);
-    });
-
-    test('can get height', () => {
-      expect(ws.height).toBe(8);
-    });
-
-    test('can set width', () => {
-      ws.width = 42;
-      expect(ws.width).toBe(42);
-    });
-
-    test('can set height', () => {
-      ws.height = 43;
-      expect(ws.height).toBe(43);
-    });
-  });
-});
-
 describe('ServerWSObject', () => {
   const DEFAULTS = Object.freeze({
     x: 0,
@@ -676,6 +586,96 @@ describe('ServerViewSpace', () => {
       expect(() => vs.moveBy(36,15)).not.toThrow();
       expect(vs.x).toBe(15);
       expect(vs.y).toBe(15);
+    });
+  });
+});
+
+describe('WorkSpace', () => {
+  const DEFAULTS = Object.freeze({
+    debug: false,
+    color: '#aaaaaa',
+    bounds: {
+      x: 10000,
+      y: 10000,
+    },
+    clientLimit: 10,
+  });
+
+  describe('constructor(port, settings)', () => {
+    test('constructs correct type of object', () => {
+      expect(new WorkSpace()).toBeInstanceOf(WorkSpace);
+    });
+
+    test('Uses default port if none provided', () => {
+      expect(new WorkSpace().port).toBe(9000);
+    });
+
+    test('Uses user-defined port, if provided', () => {
+      expect(new WorkSpace(8080).port).toBe(8080);
+    });
+
+    test('Uses port as its ID', () => {
+      const ws1 = new WorkSpace();
+      expect(ws1.port).toBe(9000);
+      expect(ws1.id).toBe(9000);
+      
+      const ws2 = new WorkSpace(1264);
+      expect(ws2.id).toBe(1264);
+      expect(ws2.port).toBe(1264);
+    });
+
+    test('ID and port are immutable', () => {
+      const ws = new WorkSpace(8080);
+      expect(ws.id).toBe(8080);
+      expect(ws.port).toBe(8080);
+      expect(ws).toHaveImmutableProperty('id');
+      expect(ws).toHaveImmutableProperty('port');
+    });
+
+    test('Uses default settings if none provided', () => {
+      expect(new WorkSpace().settings).toEqual(DEFAULTS);
+    });
+
+    test('Uses user-defined settings, if provided', () => {
+      const custom = {
+        debug: true,
+        color: 'rgb(155,72, 84)',
+        bounds: {
+          x: 1080,
+          y: 1920,
+        },
+        clientLimit: 2,
+      };
+      expect(new WorkSpace(8080, custom).settings).toEqual(custom);
+
+      const ws = new WorkSpace(8080, {clientLimit: 7});
+      expect(ws.settings).not.toEqual(DEFAULTS);
+      expect(ws.settings.debug).toEqual(DEFAULTS.debug);
+      expect(ws.settings.color).toEqual(DEFAULTS.color);
+      expect(ws.settings.bounds).toEqual(DEFAULTS.bounds);
+      expect(ws.settings.clientLimit).toBe(7);
+    });
+  });
+
+  describe('getters and setters', () => {
+    const ws = new WorkSpace(8080, {bounds: {x:7,y:8}});
+
+    test('can get width', () => {
+      expect(ws.width).toBe(7);
+    });
+
+    test('can get height', () => {
+      expect(ws.height).toBe(8);
+    });
+
+    test('can set width', () => {
+      ws.width = 42;
+      expect(ws.width).toBe(42);
+    });
+
+    test('can set height', () => {
+      ws.height = 43;
+      expect(ws.height).toBe(43);
     });
   });
 });
