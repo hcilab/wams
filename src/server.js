a8fc90053ffe581cc19206fc7253e413a77c99c1
mjv761 <michael.vanderkamp@usask.ca>
2018-07-20 09:46:55 -0600

    Reorganizing code.

--- src/server.js
+++ src/server.js
@@ -56,6 +56,245 @@ const globals = (function defineGlobals() {
   return Object.freeze(rv);
 })();
 
+const ServerWSObject = (function defineServerWSObject() {
+  const locals = Object.freeze({
+    DEFAULTS: Object.freeze({
+      x: 0,
+      y: 0,
+      width: 128,
+      height: 128,
+      type: 'view/background',
+      imgsrc: '',
+      drawCustom: '',
+      drawStart: '',
+    }),
+    STAMPER: new WamsShared.IDStamper(),
+  });
+
+  class ServerWSObject extends WamsShared.WSObject {
+    /*
+     * XXX: What is the object supposed to be if the draw strings are not 
+     *      defined?
+     *
+     * IDEA: Instead of using strings of code and running 'eval()' on them to
+     *      get custom renderings in the canvas, write a 'CanvasSequencer'
+     *      class which can pass queues of canvas operations around, checking
+     *      their legitimacy, then will run those operations on the canvas
+     *      context.
+     *      + This eliminates the arbitrary code problem, and allows us to
+     *        write an API for this behaviour through which we can exercise
+     *        some control over what is done with our WAMS API.
+     *      + For example, a sequence could look like:
+     *        
+     *          const seq = [
+     *            ['beginPath', []],
+     *            ['arc', [64, 164, 100, 2 * Math.PI, false]],
+     *            ['fillStyle', 'white'],
+     *            ['fill', []],
+     *            ['lineWidth', 5],
+     *            ['strokeStyle', '#003300'],
+     *            ['stroke', []],
+     *            ['font', 'normal 36px Verdana'],
+     *            ['fillStyle', '#000000'],
+     *            ['fillText', ['HTML5 Canvas Text', 132, 182]],
+     *          ];
+     *
+     *      + This sequence could be exucuted in the client as follows (assume
+     *        a CanvasRenderingContext2D is stored in ctx):
+     *        
+     *          seq.forEach( ([p,v]) => {
+     *            if (v instanceof Array) {
+     *              ctx[p].apply(ctx, v);
+     *            } else {
+     *              ctx[p] = v;
+     *            }
+     *          });
+     *
+     *      + The above example is rudimentary, but showcases the fundamental
+     *        idea. The 'seq' array can easily be passed between server and
+     *        client.
+     */
+    constructor(values = {}) {
+      super(WamsShared.initialize(locals.DEFAULTS, values));
+      locals.STAMPER.stamp(this);
+    }
+
+    containsPoint(x,y) {
+      return  (this.x <= x) && 
+        (this.y <= y) && 
+        (this.x + this.width  >= x) && 
+        (this.y + this.height >= y);
+    }
+
+    /*
+     * Items are allowed to be moved off screen, so limitations on where
+     * items can be moved to.
+     */
+    moveTo(x = this.x, y = this.y) {
+      this.assign({x,y});
+    }
+
+    moveBy(dx = 0, dy = 0) {
+      this.moveTo(this.x + dx, this.y + dy);
+    }
+  }
+
+  return ServerWSObject;
+})();
+
+const ServerViewSpace = (function defineServerViewSpace() {
+  const locals = Object.freeze({
+    DEFAULTS: {
+      x: 0,
+      y: 0,
+      width: 1600,
+      height: 900,
+      type: 'view/background',
+      effectiveWidth: 1600,
+      effectiveHeight: 900,
+      scale: 1,
+      rotation: 0,
+    },
+    MIN_DIMENSION: 100,
+    STAMPER: new WamsShared.IDStamper(),
+
+    resolveBounds(bounds = {}) {
+      function safeNumber(x) {
+        return Number(x) || 0; // Prevents NaN from falling through.
+      }
+      const x = safeNumber(bounds.x);
+      const y = safeNumber(bounds.y);
+      if (x < 100 || y < 100) throw 'Invalid bounds received';
+      return {x,y};
+    }
+  });
+
+  class ServerViewSpace extends WamsShared.ViewSpace {
+    /*
+     * XXX: At some point, the effective width and height should be made to be
+     *      updated whenever either the width, height, or scale of the
+     *      viewspace get updated. This could be achieve with getters and 
+     *      setters on those three values. Might need to think through all the
+     *      possible complications though.
+     *
+     *      The same thing could maybe be done with the 'center' getter, so
+     *      that it refers to an actual stored value that gets updated whenever
+     *      the effective width or height gets updated, or when the x or y
+     *      values get updated. This would prevent having to recompute every
+     *      time the value is accessed, which is the way things are working
+     *      currently.
+     *
+     *      Perhaps one technique would be to find a way of storing the actual
+     *      x, y, width, height, effectiveWidth, effectiveHeight, and scale
+     *      using some private data technique with alternative names for the
+     *      variables (or some other storage method) and have the original 
+     *      names be used for the getters and setters. Might want to have a
+     *      look at the shared Reporter factory definition to see if this can
+     *      be handled at a more general level.
+     */
+    constructor(bounds, values) {
+      super(WamsShared.initialize(locals.DEFAULTS, values));
+      this.bounds = locals.resolveBounds(bounds);
+      this.effectiveWidth = this.width / this.scale;
+      this.effectiveHeight = this.height / this.scale;
+      locals.STAMPER.stamp(this);
+    }
+
+    get bottom()  { return this.y + this.effectiveHeight; }
+    get left()    { return this.x; }
+    get right()   { return this.x + this.effectiveWidth; }
+    get top()     { return this.y; }
+
+    /*
+     * The center() getter returns an object that exposes x and y getters which
+     * will always return the _current_ (at the moment the getter is called)
+     * center of the viewspace along that dimension.
+     */
+    get center()  {
+      return ((view) => {
+        return Object.freeze({
+          get x() { return view.x + (view.effectiveWidth  / 2); },
+          get y() { return view.y + (view.effectiveHeight / 2); },
+        });
+      })(this);
+    }
+
+    canBeScaledTo(width = this.width, height = this.height) {
+      return  (width  > 0) &&
+        (height > 0) &&
+        (this.x + width  <= this.bounds.x) &&
+        (this.y + height <= this.bounds.y);
+    }
+
+    /*
+     * The canMoveTo[XY] functions are split up in order to allow for the x and
+     * y dimensions to be independently moved. In other words, if a move fails
+     * in the x direction, it can still succeed in the y direction. This makes
+     * it easier to push the viewspace into the boundaries.
+     *
+     * XXX: Can they be unified simply while still allowing this kind of 
+     *      separation?
+     */
+    canMoveToX(x = this.x) {
+      return (x >= 0) && (x + this.effectiveWidth <= this.bounds.x);
+    }
+
+    canMoveToY(y = this.y) {
+      return (y >= 0) && (y + this.effectiveHeight <= this.bounds.y);
+    }
+
+    /*
+     * ViewSpaces are constrained to stay within the boundaries of the
+     * workspace, to protect the render. To ensure this safety, extra
+     * potentially redundant checks and fallbacks are used in this function.
+     */
+    moveTo(x = this.x, y = this.y) {
+      const coordinates = {
+        x: this.x, 
+        y: this.y
+      };
+      if (this.canMoveToX(x)) coordinates.x = x;
+      if (this.canMoveToY(y)) coordinates.y = y;
+      this.assign(coordinates);
+    }
+
+    moveBy(dx = 0, dy = 0) {
+      this.moveTo(this.x + dx, this.y + dy);
+    }
+
+    /*
+     * The scaled width and height (stored permanently as effective width and
+     * height) are determined by dividing the width or height by the scale.
+     * This might seem odd at first, as that seems to be the opposite of what
+     * should be done. But what these variables are actually representing is 
+     * the amount of the underlying workspace that can be displayed inside the
+     * viewspace. So a larger scale means that each portion of the workspace
+     * takes up more of the viewspace, therefore _less_ of the workspace is
+     * visible. Hence, division.
+     *
+     * XXX: One thing that could be done in this function is to try anchoring
+     *      on the right / bottom if anchoring on the left / top produces a
+     *      failure. (By anchoring, I mean that the given position remains
+     *      constant while the scaling is occurring).
+     */
+    rescale(scale = this.scale) {
+      const scaledWidth = this.width / scale;
+      const scaledHeight = this.height / scale;
+      if (this.canBeScaledTo(scaledWidth, scaledHeight)) {
+        this.assign({
+          scale: scale,
+          effectiveWidth: scaledWidth,
+          effectiveHeight: scaledHeight,
+        });
+        return true;
+      }
+      return false;
+    }
+  }
+
+  return ServerViewSpace;
+})();
+
 /*
  * XXX: The paths being used in the request handler are a little hacky and all
  *      over the place right now. Look into how to normalize them.
@@ -437,245 +676,6 @@ const Connection = (function defineConnection() {
   }
 
   return Connection;
-})();
-
-const ServerWSObject = (function defineServerWSObject() {
-  const locals = Object.freeze({
-    DEFAULTS: Object.freeze({
-      x: 0,
-      y: 0,
-      width: 128,
-      height: 128,
-      type: 'view/background',
-      imgsrc: '',
-      drawCustom: '',
-      drawStart: '',
-    }),
-    STAMPER: new WamsShared.IDStamper(),
-  });
-
-  class ServerWSObject extends WamsShared.WSObject {
-    /*
-     * XXX: What is the object supposed to be if the draw strings are not 
-     *      defined?
-     *
-     * IDEA: Instead of using strings of code and running 'eval()' on them to
-     *      get custom renderings in the canvas, write a 'CanvasSequencer'
-     *      class which can pass queues of canvas operations around, checking
-     *      their legitimacy, then will run those operations on the canvas
-     *      context.
-     *      + This eliminates the arbitrary code problem, and allows us to
-     *        write an API for this behaviour through which we can exercise
-     *        some control over what is done with our WAMS API.
-     *      + For example, a sequence could look like:
-     *        
-     *          const seq = [
-     *            ['beginPath', []],
-     *            ['arc', [64, 164, 100, 2 * Math.PI, false]],
-     *            ['fillStyle', 'white'],
-     *            ['fill', []],
-     *            ['lineWidth', 5],
-     *            ['strokeStyle', '#003300'],
-     *            ['stroke', []],
-     *            ['font', 'normal 36px Verdana'],
-     *            ['fillStyle', '#000000'],
-     *            ['fillText', ['HTML5 Canvas Text', 132, 182]],
-     *          ];
-     *
-     *      + This sequence could be exucuted in the client as follows (assume
-     *        a CanvasRenderingContext2D is stored in ctx):
-     *        
-     *          seq.forEach( ([p,v]) => {
-     *            if (v instanceof Array) {
-     *              ctx[p].apply(ctx, v);
-     *            } else {
-     *              ctx[p] = v;
-     *            }
-     *          });
-     *
-     *      + The above example is rudimentary, but showcases the fundamental
-     *        idea. The 'seq' array can easily be passed between server and
-     *        client.
-     */
-    constructor(values = {}) {
-      super(WamsShared.initialize(locals.DEFAULTS, values));
-      locals.STAMPER.stamp(this);
-    }
-
-    containsPoint(x,y) {
-      return  (this.x <= x) && 
-        (this.y <= y) && 
-        (this.x + this.width  >= x) && 
-        (this.y + this.height >= y);
-    }
-
-    /*
-     * Items are allowed to be moved off screen, so limitations on where
-     * items can be moved to.
-     */
-    moveTo(x = this.x, y = this.y) {
-      this.assign({x,y});
-    }
-
-    moveBy(dx = 0, dy = 0) {
-      this.moveTo(this.x + dx, this.y + dy);
-    }
-  }
-
-  return ServerWSObject;
-})();
-
-const ServerViewSpace = (function defineServerViewSpace() {
-  const locals = Object.freeze({
-    DEFAULTS: {
-      x: 0,
-      y: 0,
-      width: 1600,
-      height: 900,
-      type: 'view/background',
-      effectiveWidth: 1600,
-      effectiveHeight: 900,
-      scale: 1,
-      rotation: 0,
-    },
-    MIN_DIMENSION: 100,
-    STAMPER: new WamsShared.IDStamper(),
-
-    resolveBounds(bounds = {}) {
-      function safeNumber(x) {
-        return Number(x) || 0; // Prevents NaN from falling through.
-      }
-      const x = safeNumber(bounds.x);
-      const y = safeNumber(bounds.y);
-      if (x < 100 || y < 100) throw 'Invalid bounds received';
-      return {x,y};
-    }
-  });
-
-  class ServerViewSpace extends WamsShared.ViewSpace {
-    /*
-     * XXX: At some point, the effective width and height should be made to be
-     *      updated whenever either the width, height, or scale of the
-     *      viewspace get updated. This could be achieve with getters and 
-     *      setters on those three values. Might need to think through all the
-     *      possible complications though.
-     *
-     *      The same thing could maybe be done with the 'center' getter, so
-     *      that it refers to an actual stored value that gets updated whenever
-     *      the effective width or height gets updated, or when the x or y
-     *      values get updated. This would prevent having to recompute every
-     *      time the value is accessed, which is the way things are working
-     *      currently.
-     *
-     *      Perhaps one technique would be to find a way of storing the actual
-     *      x, y, width, height, effectiveWidth, effectiveHeight, and scale
-     *      using some private data technique with alternative names for the
-     *      variables (or some other storage method) and have the original 
-     *      names be used for the getters and setters. Might want to have a
-     *      look at the shared Reporter factory definition to see if this can
-     *      be handled at a more general level.
-     */
-    constructor(bounds, values) {
-      super(WamsShared.initialize(locals.DEFAULTS, values));
-      this.bounds = locals.resolveBounds(bounds);
-      this.effectiveWidth = this.width / this.scale;
-      this.effectiveHeight = this.height / this.scale;
-      locals.STAMPER.stamp(this);
-    }
-
-    get bottom()  { return this.y + this.effectiveHeight; }
-    get left()    { return this.x; }
-    get right()   { return this.x + this.effectiveWidth; }
-    get top()     { return this.y; }
-
-    /*
-     * The center() getter returns an object that exposes x and y getters which
-     * will always return the _current_ (at the moment the getter is called)
-     * center of the viewspace along that dimension.
-     */
-    get center()  {
-      return ((view) => {
-        return Object.freeze({
-          get x() { return view.x + (view.effectiveWidth  / 2); },
-          get y() { return view.y + (view.effectiveHeight / 2); },
-        });
-      })(this);
-    }
-
-    canBeScaledTo(width = this.width, height = this.height) {
-      return  (width  > 0) &&
-        (height > 0) &&
-        (this.x + width  <= this.bounds.x) &&
-        (this.y + height <= this.bounds.y);
-    }
-
-    /*
-     * The canMoveTo[XY] functions are split up in order to allow for the x and
-     * y dimensions to be independently moved. In other words, if a move fails
-     * in the x direction, it can still succeed in the y direction. This makes
-     * it easier to push the viewspace into the boundaries.
-     *
-     * XXX: Can they be unified simply while still allowing this kind of 
-     *      separation?
-     */
-    canMoveToX(x = this.x) {
-      return (x >= 0) && (x + this.effectiveWidth <= this.bounds.x);
-    }
-
-    canMoveToY(y = this.y) {
-      return (y >= 0) && (y + this.effectiveHeight <= this.bounds.y);
-    }
-
-    /*
-     * ViewSpaces are constrained to stay within the boundaries of the
-     * workspace, to protect the render. To ensure this safety, extra
-     * potentially redundant checks and fallbacks are used in this function.
-     */
-    moveTo(x = this.x, y = this.y) {
-      const coordinates = {
-        x: this.x, 
-        y: this.y
-      };
-      if (this.canMoveToX(x)) coordinates.x = x;
-      if (this.canMoveToY(y)) coordinates.y = y;
-      this.assign(coordinates);
-    }
-
-    moveBy(dx = 0, dy = 0) {
-      this.moveTo(this.x + dx, this.y + dy);
-    }
-
-    /*
-     * The scaled width and height (stored permanently as effective width and
-     * height) are determined by dividing the width or height by the scale.
-     * This might seem odd at first, as that seems to be the opposite of what
-     * should be done. But what these variables are actually representing is 
-     * the amount of the underlying workspace that can be displayed inside the
-     * viewspace. So a larger scale means that each portion of the workspace
-     * takes up more of the viewspace, therefore _less_ of the workspace is
-     * visible. Hence, division.
-     *
-     * XXX: One thing that could be done in this function is to try anchoring
-     *      on the right / bottom if anchoring on the left / top produces a
-     *      failure. (By anchoring, I mean that the given position remains
-     *      constant while the scaling is occurring).
-     */
-    rescale(scale = this.scale) {
-      const scaledWidth = this.width / scale;
-      const scaledHeight = this.height / scale;
-      if (this.canBeScaledTo(scaledWidth, scaledHeight)) {
-        this.assign({
-          scale: scale,
-          effectiveWidth: scaledWidth,
-          effectiveHeight: scaledHeight,
-        });
-        return true;
-      }
-      return false;
-    }
-  }
-
-  return ServerViewSpace;
 })();
 
 exports.WorkSpace = WorkSpace;
