67807a01d7a931b3404b8052c45128ada8c2e62d
mjv761 <michael.vanderkamp@usask.ca>
2019-01-04 23:07:29 -0600

    Add generic polygon to predefined item set

--- src/predefined/items.js
+++ src/predefined/items.js
@@ -12,7 +12,8 @@ const { CanvasBlueprint } = require('canvas-sequencer');
 const Polygon2D  = require('../server/Polygon2D.js');
 
 /**
- * Returns a rectangular item with the given width and height.
+ * Returns an object with the parameters for a rectangular item with the given
+ * width and height.
  */
 function rectangle(x, y, width, height, type = 'rectangle', colour = 'blue') {
   const hitbox = new Polygon2D([
@@ -23,19 +24,40 @@ function rectangle(x, y, width, height, type = 'rectangle', colour = 'blue') {
   ]);
   const blueprint = new CanvasBlueprint();
   blueprint.fillStyle = colour;
-  blueprint.fillRect('{x}', '{y}', width, height);
+  blueprint.fillRect(0, 0, width, height);
   
   return { x, y, hitbox, type, blueprint };
 }
 
 /**
- * Returns a square item with the given side length.
+ * Returns an object with the parameters for a square item with the given width
+ * and height.
  */
 function square(x, y, length, type = 'square', colour = 'red') {
   return rectangle(x, y, length, length, type, colour);
 }
 
+/**
+ * Returns an object with the parameters for a generic polygon.
+ */
+function polygon(x, y, points = [], type = 'polygon', colour = 'green') {
+  if (points.length < 3) return null;
+
+  const hitbox = new Polygon2D(points);
+
+  const blueprint = new CanvasBlueprint();
+  blueprint.fillStyle = colour;
+  blueprint.beginPath();
+  blueprint.moveTo(points[0].x, points[0].y);
+  points.forEach( p => blueprint.lineTo(p.x, p.y));
+  blueprint.closePath();
+  blueprint.fill();
+
+  return { x, y, hitbox, type, blueprint };
+}
+
 module.exports = {
+  polygon,
   rectangle,
   square,
 };
