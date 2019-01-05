040710be028684ec5dd73316d3ec7c3c59a86883
mjv761 <michael.vanderkamp@usask.ca>
2019-01-05 09:28:37 -0600

    Fix ClientItem draw() sequence for images

--- src/client/ClientItem.js
+++ src/client/ClientItem.js
@@ -87,32 +87,16 @@ class ClientItem extends Item {
    * context: CanvasRenderingContext2D onto which to draw this item.
    */
   draw(context) {
-    // const width = this.width || this.img.width;
-    // const height = this.height || this.img.height;
-
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(constants.ROTATE_360 - this.rotation);
+    context.scale(this.scale, this.scale);
     if (this.sequence) {
-      context.save();
-      context.scale(this.scale, this.scale);
-      context.rotate(constants.ROTATE_360 - this.rotation);
-      context.translate(this.x, this.y);
       this.sequence.execute(context);
-      context.restore();
     } else if (this.img && this.img.loaded) {
-      context.drawImage(
-        this.img,
-        this.x,
-        this.y,
-        this.img.width,
-        this.img.height
-      );
+      context.drawImage( this.img, 0, 0, this.img.width, this.img.height );
     } 
-    // else {
-    //   // Draw placeholder rectangle.
-    //   context.save();
-    //   context.fillStyle = '#252525';
-    //   context.fillRect(this.x, this.y, width, height);
-    //   context.restore();
-    // }
+    context.restore();
   }
 }
 
