3224da07ae7d8d57f569fedb56564961a17a0060
mjv761 <michael.vanderkamp@usask.ca>
2019-01-09 22:41:50 -0600

    Update drawing example. Item props match view
    Specifically, scale and rotation now match

--- examples/drawing.js
+++ examples/drawing.js
@@ -19,21 +19,39 @@ const colours = [
   'lime',
 ];
 
-function square(x, y, index) {
-  return Wams.predefined.items.square(128, colours[index % colours.length], {
+function square2(x, y, view) {
+  return Wams.predefined.items.polygon([
+      { x: -64, y: -64 },
+      { x: 64, y: -64 },
+      { x: 64, y: 64 },
+      { x: -64, y: 64 },
+    ], 
+    colours[view.id % colours.length],
+    {
+      x,
+      y,
+      type: 'colour',
+      scale: 1 / view.scale,
+      rotation: view.rotation,
+    }
+  );
+}
+
+function square(x, y, view) {
+  return Wams.predefined.items.square(128, colours[view.id % colours.length], {
     // x: ix - 64,
     // y: iy - 64,
     x,
     y,
     type: 'colour',
-    scale: Math.random() + 0.5,
-    rotation: Math.random() * 2 * Math.PI,
+    scale: 1 / view.scale,
+    rotation: view.rotation,
   });
 }
 
 // Attaches the different function handlers
 ws.on('layout', Wams.predefined.layout.placeAtXY(ws, 4000, 4000));
-ws.on('click',  Wams.predefined.tap.spawnOrRemoveItem(ws, square, 'colour'));
+ws.on('click',  Wams.predefined.tap.spawnOrRemoveItem(ws, square2, 'colour'));
 ws.on('scale',  Wams.predefined.scale.view(ws));
 ws.on('drag',   Wams.predefined.drag.itemsAndView(ws, ['colour']));
 ws.on('rotate', Wams.predefined.rotate.view(ws));
