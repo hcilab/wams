375cc4dbeac02a6432e6b66bae2128eb5241b39d
mjv761 <michael.vanderkamp@usask.ca>
2019-02-02 14:01:20 -0600

    Use node_modules version of westures
    As we have updated it...

--- src/client/Interactor.js
+++ src/client/Interactor.js
@@ -10,8 +10,8 @@
 
 'use strict';
 
-const Westures = require('../../../westures');
-// const Westures = require('westures');
+// const Westures = require('../../../westures');
+const Westures = require('westures');
 const { mergeMatches, NOP } = require('../shared.js');
 
 const HANDLERS = Object.freeze({ 
