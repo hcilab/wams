04bd3efdf9b24a2b5de44384a611c72996e42c6b
mjv761 <michael.vanderkamp@usask.ca>
2019-02-07 11:00:02 -0600

    Update more jsdoc commends in predefined module

--- src/predefined/items.js
+++ src/predefined/items.js
@@ -12,6 +12,26 @@ const sizeOfImage = require('image-size');
 
 const Polygon2D  = require('../server/Polygon2D.js');
 
+/**
+ * Factories for predefined items.
+ *
+ * @namespace items
+ * @memberof module:predefined
+ */
+
+/**
+ * Applies rotation and scale to a hitbox, if necessary.
+ *
+ * @inner
+ * @memberof module:predefined.items
+ *
+ * @param {Object} data
+ * @param {module:server.Polygon2D} [data.hitbox]
+ * @param {number} [data.rotation]
+ * @param {number} [data.scale]
+ *
+ * @returns {Object} The appropriately transformed data.
+ */
 function transformed(data = {}) {
   if (data.hitbox) {
     if (data.rotation)  data.hitbox.rotate(-data.rotation);
@@ -21,8 +41,18 @@ function transformed(data = {}) {
 }
 
 /**
- * Returns a 4-point polygon representing a rectangle anchored at (0,0) and with
- * the given width and height.
+ * Generates a rectangular hitbox.
+ *
+ * @inner
+ * @memberof module:predefined.items
+ *
+ * @param {number} x
+ * @param {number} y
+ * @param {number} width
+ * @param {number} height
+ *
+ * @returns {module:server.Polygon2D} A 4-point polygon representing a rectangle
+ * anchored at (0,0) and with the given width and height.
  */
 function rectangularHitbox(x, y, width, height) {
   return new Polygon2D([
@@ -34,13 +64,20 @@ function rectangularHitbox(x, y, width, height) {
 }
 
 /**
- * Returns an object with the parameters for an image item using the given
- * source.
  *
  * NOTE: This function should only be used for images that are intended to be
  * interactable! If you want an image that shouldn't be interacted with, simply
  * spawn an item directly with the itemOptions (including the imgsrc as a
  * property) that you would have supplied here.
+ *
+ * @memberof module:predefined.items
+ *
+ * @param {string} imgsrc - Route path to the image.
+ * @param {Object} itemOptions - Location and orientation options for the image
+ * item. See {@link module:shared.Item} members for available parameters.
+ *
+ * @returns {Object} An object with the parameters for an image item using the
+ * given source.
  */
 function image(imgsrc, itemOptions = {}) {
   const dims = sizeOfImage(imgsrc);
@@ -49,8 +86,20 @@ function image(imgsrc, itemOptions = {}) {
 }
 
 /**
- * Returns an object with the parameters for a rectangular item with the given
- * width and height, filled in with the given colour.
+ * Generate a rectangular block item.
+ *
+ * @memberof module:predefined.items
+ *
+ * @param {number} x
+ * @param {number} y
+ * @param {number} width
+ * @param {number} height
+ * @param {string} [colour='blue'] - Fill colour for the rectangle.
+ * @param {Object} itemOptions - Location and orientation options for the item.
+ * See {@link module:shared.Item} members for available parameters.
+ *
+ * @returns {Object} An object with the parameters for a rectangular item with
+ * the given width and height, filled in with the given colour.
  */
 function rectangle(x, y, width, height, colour = 'blue', itemOptions = {}) {
   const hitbox = rectangularHitbox(x, y, width, height);
@@ -62,16 +111,37 @@ function rectangle(x, y, width, height, colour = 'blue', itemOptions = {}) {
 }
 
 /**
- * Returns an object with the parameters for a square item with the given side
- * length, filled in with the given colour.
+ * Generate a square block item.
+ *
+ * @memberof module:predefined.items
+ *
+ * @param {number} x
+ * @param {number} y
+ * @param {number} length
+ * @param {string} [colour='red'] - Fill colour for the square.
+ * @param {Object} itemOptions - Location and orientation options for the item.
+ * See {@link module:shared.Item} members for available parameters.
+ *
+ * @returns {Object} An object with the parameters for a square item with the
+ * given side length, filled in with the given colour.
  */
 function square(x, y, length, colour = 'red', itemOptions = {}) {
   return rectangle(x, y, length, length, colour, itemOptions);
 }
 
 /**
- * Returns an object with the parameters for a generic polygon, filled in with
- * the given colour.
+ * Generate a polygonal item.
+ *
+ * @memberof module:predefined.items
+ *
+ * @param {module:server.Point2D[]} points - Not necessarily actual Point2D
+ * objects, can just be objects with x and y properties.
+ * @param {string} [colour='green'] - Fill colour for the polygon.
+ * @param {Object} itemOptions - Location and orientation options for the item.
+ * See {@link module:shared.Item} members for available parameters.
+ *
+ * @returns {Object} An object with the parameters for a generic polygon, filled
+ * in with the given colour.
  */
 function polygon(points = [], colour = 'green', itemOptions = {}) {
   if (points.length < 3) return null;
