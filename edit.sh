951d5ef4707fbe933e5a619ea97721dd5c66d5af
mjv761 <michael.vanderkamp@usask.ca>
2019-02-20 22:51:18 -0600

    Add index file to edit.sh

--- edit.sh
+++ edit.sh
@@ -3,14 +3,15 @@
 # Author: Michael van der Kamp
 # Date: August 21, 2018
 #
-# This bash script opens a new vim instance with three windows, one of which is
-#  a small scratch buffer for reading the output of external commands.
+# This bash script opens a new vim instance with three windows, one of which
+# holds a terminal for accessing external commands.
 #
 # This file will be where I maintain my personal preference for opening up vim
 #  when working on a project. This is intended as a template, but is perfectly
 #  usable if a project follows this simple layout.
 
 vim \
+  index.js \
   src/*.js \
   src/client/*.js \
   src/server/*.js \
@@ -24,6 +25,9 @@ vim \
   README.md \
   TODO.md \
   DESIGN.md \
+  .eslintrc.json \
+  .jsdocrc.json \
+  package.json \
   "+vertical botright terminal" \
   "+aboveleft new" \
   "+argument 2" \
