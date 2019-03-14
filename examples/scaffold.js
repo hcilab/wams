071fdebac5ba85e9cf7b0d2d15526eb086e90129
mjv761 <michael.vanderkamp@usask.ca>
2019-03-13 23:08:21 -0600

    Touch up scaffold example

--- examples/scaffold.js
+++ examples/scaffold.js
@@ -6,30 +6,35 @@
 const Wams = require('..');
 const app = new Wams.Application();
 
-const handleLayout = function(view, numViews) {
+function handleLayout(view, position, device) {
   // Executed once every time a new user joins
-};
+}
 
-const handleClick = function(view, target, x, y) {
+function handleClick(view, target, x, y) {
   // Executed every time a user taps or clicks a screen
-};
+}
 
-const handleDrag = function(view, target, x, y, dx, dy) {
+function handleDrag(view, target, x, y, dx, dy) {
   // Executed every time a drag occurs on a device
-};
+}
 
-const handleScale = function(view, scale, mx, my) {
-  // Executed when a user pinches a device, or uses the scroll wheel on a
-  // computer
-};
+function handleRotate(view, target, radians, x, y) {
+  // Executed every time a multitouch rotate is detected, or if a desktop user
+  // holds CTRL while holding down the mouse button and dragging.
+}
+
+function handleScale(view, target, scale, x, y) {
+  // Executed when a user pinches a device, or uses the scroll wheel on a mouse.
+}
 
 // Attaches the defferent function handlers
 app.on('click',  handleClick);
 app.on('drag',   handleDrag);
+app.on('rotate', handleRotate);
 app.on('scale',  handleScale);
 app.on('layout', handleLayout);
 
 // Once all the handlers are attached, open up the workspace and listen for
 // connections.
-app.listen(9004);
+app.listen(8080);
 
