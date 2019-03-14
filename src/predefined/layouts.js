dd95386fab787b7e6bd5f1ebf6a169910bc609fa
mjv761 <michael.vanderkamp@usask.ca>
2019-03-13 22:01:40 -0600

    Add a 'line' predefined layout

--- src/predefined/layouts.js
+++ src/predefined/layouts.js
@@ -15,7 +15,47 @@
  */
 
 /**
- * Generates a handler
+ * Generates a handler that places users in a line, with the given amount of
+ * overlap. Best used with either server-side gestures or when users are unable
+ * to manipulate their views.
+ * - Valid for use with server-side gestures.
+ *
+ * @memberof module:predefined.layouts
+ *
+ * @param {number} overlap
+ *
+ * @returns {module:server.ListenerTypes.LayoutListener} A WAMS layout handler
+ * function that places users in a line.
+ */
+function line(overlap) {
+  const views = [];
+  const rights = [];
+
+  function layout(view, position, device) {
+    if (position > 0) {
+      if (views[position - 1] == null) {
+        setTimeout(() => layout(view, position, device), 0);
+      } else {
+        const prev = views[position - 1];
+        const change = prev.transformPointChange(overlap, 0);
+        const anchor = prev.topRight.minus(change);
+        view.moveTo(anchor.x, anchor.y);
+        const side = rights[position - 1] - overlap;
+        device.moveTo(side, 0);
+        rights[position] = side + device.width;
+        views[position] = view;
+      }
+    } else {
+      rights[0] = device.width;
+      views[position] = view;
+    }
+  }
+
+  return layout;
+}
+
+/**
+ * Generates a handler that places all new users at the given coordinates.
  *
  * @memberof module:predefined.layouts
  *
@@ -32,6 +72,7 @@ function placeAtXY(x, y) {
 }
 
 module.exports = {
+  line,
   placeAtXY,
 };
 
