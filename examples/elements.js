1fa8d069ba31ef35c2e13a030c235b40109a7f0b
mjv761 <michael.vanderkamp@usask.ca>
2019-03-20 20:01:27 -0600

    Initial commit of 'elements' example
    Demonstrates using HTML elements in the system!!

---
+++ examples/elements.js
@@ -0,0 +1,43 @@
+/*
+ * This is a simple example showing how users can interact with a shared set
+ *  of items.
+ */
+
+'use strict';
+
+const Wams = require('..');
+const app = new Wams.Application();
+
+function element(x, y, view) {
+  return Wams.predefined.items.element(
+    0,
+    0,
+    300,
+    50,
+    {
+      x,
+      y,
+      width: 300,
+      height: 50,
+      type:  'button',
+      scale: 1 / view.scale,
+      rotation: view.rotation,
+      tagname: 'button',
+      attributes: {
+        innerText: 'This is an HTMLButtonElement!',
+      },
+    }
+  );
+}
+
+// Attaches the different function handlers
+app.on('scale',  Wams.predefined.scales.itemsAndView(['button']));
+app.on('drag',   Wams.predefined.drags.itemsAndView(['button']));
+app.on('rotate', Wams.predefined.rotates.itemsAndView(['button']));
+app.on(
+  'click',
+  Wams.predefined.taps.spawnOrRemoveItem(app, element, 'button', 'spawnElement')
+);
+
+app.listen(9002);
+
