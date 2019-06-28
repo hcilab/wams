1c6f56910ac58758c7a985716659911b61f62368
Nick Baliesnyi <nikita.balesny@gmail.com>
2019-06-28 08:28:46 -0300

    update router to route images folder by default

--- src/server/Router.js
+++ src/server/Router.js
@@ -26,12 +26,14 @@ function Router() {
   const app = express();
 
   // Establish main routes.
-  const view   = path.join(__dirname, '../../dist/index.html');
+  const view = path.join(__dirname, '../../dist/index.html');
   app.get('/', (req, res) => res.sendFile(view));
 
   app.use('/wams', express.static(path.join(__dirname, '../../dist/wams')))
 
   app.use('/app', express.static(path.join(__dirname, '../../dist/app')))
+
+  app.use('/images', express.static(path.join(__dirname, '../../img')));
 
   // Make the express object accessible (e.g. for express.static())
   app.express = express;
