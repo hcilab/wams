abf614cb66d8e6f6d6c38115de0def41f240684f
Nick Baliesnyi <nikita.balesny@gmail.com>
2019-08-22 12:14:14 -0300

    add color picking to drawing app

--- examples/client/drawing-app.js
+++ examples/client/drawing-app.js
@@ -3,38 +3,91 @@ root.id = 'root'
 document.querySelector('body').appendChild(root)
 
 const states = ['pan', 'draw']
-const currentState = 0
+let currentState = 0
 
-root.innerHTML += `
-    <div id="controls">
-        <ul class="control-buttons">
-            <div class="button ${currentState === 0 ? 'active' : ''}" data-type="pan">
-                <i class="far fa-hand-paper"></i>
-            </div>
-            <div class="button ${currentState === 1 ? 'active' : ''}" data-type="draw">
-                <i class="fas fa-pencil-alt"></i>
-            </div>
-        </ul>
-    </div> 
+// rainbow colors + black + white
+const RAINBOW_COLORS = ['red', 'orange', 'yellow', 'green', 'blue', ]
+const listOfColors = [...RAINBOW_COLORS, 'white', 'black']
+let currentColor = 0
+
+
+const CONTROLS = () => `
+<div id="controls">
+    <ul class="control-buttons">
+        <div id="pan" class="button ${currentState === 0 ? 'active' : ''}" data-type="pan">
+            <i class="far fa-hand-paper"></i>
+        </div>
+        <div id="draw" class="button ${currentState === 1 ? 'active' : ''}" data-type="draw">
+            <i class="fas fa-pencil-alt"></i>
+        </div>
+        <div id="color" class="button ${listOfColors[currentColor]}" data-type="color">
+            <i class="fas fa-circle"></i>
+        </div>
+        <div id="color-picker">
+            ${listOfColors.map(color => `
+                <i class="fas fa-circle ${color}" data-color="${color}"></i>            
+            `).join('')}
+        </div>
+    </ul>
+</div>
 `
+
+function renderControls() {
+    root.innerHTML = CONTROLS()
+    initControlsListeners()
+}
 
 function initControlsListeners() {
     const buttons = document.querySelectorAll('.button')
-    buttons.forEach(el => {
-        el.addEventListener('click', (event) => {
-            const button = event.target.closest('.button')
-            const buttons = document.querySelectorAll('.button');
-            [].forEach.call(buttons, (el) => {
-                el.classList.remove('active')
-            })
-            button.classList.add('active')
+    const panBtn = document.querySelector('#pan')
+    const drawBtn = document.querySelector('#draw')
+    const colorBtn = document.querySelector('#color')
+    const colorPicker = document.querySelector('#color-picker')
+    const colors = document.querySelectorAll('#color-picker > *')
 
-            const controlType = button.dataset.type
-            WAMS.dispatch('set-control', controlType)
+    function chooseControlType(event) {
+        const button = event.target.closest('.button')
+        forEachEl(buttons, (el) => {
+            el.classList.remove('active')
+        })
+        button.classList.add('active')
+
+        const type = button.dataset.type
+        currentState = states.indexOf(type)
+        WAMS.dispatch('set-control', type)
+    }
+
+    addClickTouchListener(panBtn, event => {
+        chooseControlType(event)
+    })
+
+    addClickTouchListener(drawBtn, event => {
+        chooseControlType(event)
+    })
+
+    addClickTouchListener(colorBtn, event => {
+        colorPicker.classList.toggle('show')
+    })
+
+    forEachEl(colors, el => {
+        addClickTouchListener(el, event => {
+            const color = event.target.dataset.color
+            colorPicker.classList.remove('show')
+            currentColor = listOfColors.indexOf(color)
+            WAMS.dispatch('set-color', color)
+            renderControls()
         })
     })
 
 }
 
+function forEachEl(elements, callback) {
+    [].forEach.call(elements, callback)
+}
 
-initControlsListeners()
+function addClickTouchListener(el, callback) {
+    el.addEventListener('click', callback)
+    el.addEventListener('touch', callback)
+}
+
+renderControls()
--- examples/drawing-app.js
+++ examples/drawing-app.js
@@ -9,19 +9,17 @@ const path = require('path')
 const { html, square } = WAMS.predefined.items
 const { actions } = WAMS.predefined
 
-// markup for video player controls
-const controlsHtml = ({ pan, draw }) => (
-    `<div id="controls">
-        <ul class="control-buttons">
-            <div class="button">
-                <i class="far fa-hand-paper ${pan ? 'active' : ''}"></i>
-            </div>
-            <div class="button">
-                <i class="fas fa-pencil-alt ${draw ? 'active' : ''}"></i>
-            </div>
-        </ul>
-    </div>`
-)
+const COLORS = {
+    red:    '#D12C1F',
+    orange: '#EF9135',
+    yellow: '#FBEE4F',
+    green:  '#377F34',
+    blue:   '#1E4CF5',
+    purple: '#6C1684',
+    white:  '#fff',
+    grey:   '#808080',
+    black:  '#000',
+}
 
 class DrawingApp {
     constructor() {
@@ -39,7 +37,14 @@ class DrawingApp {
             staticDir: path.join(__dirname, './client'),
         })
 
+        this.setColor(COLORS['red'])
+
         this.initListeners()
+    }
+
+    setColor(color) {
+        this.currentColor = COLORS['red']
+        this.app.workspace.state.color = color
     }
 
     initListeners() {
@@ -48,6 +53,10 @@ class DrawingApp {
         this.app.on('set-control', (type, view) => {
             this.updateControlType(type, view)
         })
+
+        this.app.on('set-color', (color) => {
+            this.setColor(COLORS[color])
+        })
         this.app.spawn(square(200, 200, 100, '#555'))
         this.app.onconnect(this.handleConnect.bind(this))
         this.app.listen(8080)
@@ -55,7 +64,6 @@ class DrawingApp {
 
     updateControlType(type, view) {
         this.controlType = type
-
         view.ondrag = type === 'pan' ? actions.drag : actions.draw
     }
 
@@ -72,16 +80,8 @@ class DrawingApp {
     }
 
     handleConnect(view) {
-        this.app.dispatch('init')
-        // // re-rendering controls
-        // // to show current play/pause btn status
-        // // for newly connected device
-        // if (this.controls) this.app.removeItem(this.controls)
-        // this.spawnControls(view)
-
-        // // notify clients that controls are spawned
-        // this.app.dispatch('controlsSpawned')
         view.allowDrag = true
+        view.allowScale = true
     }
 }
 
