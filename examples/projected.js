43c26f9bd2951d590afc36365d9b319b6f96c088
Nick Baliesnyi <nikita.balesny@gmail.com>
2019-08-30 13:52:13 -0300

    add projected example

---
+++ examples/projected.js
@@ -0,0 +1,34 @@
+const WAMS = require('..');
+const path = require('path')
+
+const app = new WAMS.Application({
+	shadows: true,
+	staticDir: path.join(__dirname, './img'),
+	useMultiScreenGestures: true
+});
+
+const { image } = WAMS.predefined.items;
+
+app.on('position', (data) => {
+	console.log(data)
+})
+
+app.spawn(image('map.jpg', {
+	width: 5650, height: 6053,
+	x: 0, y: 0,
+}));
+
+function viewSetup(view, device, group) {
+	if (view.index == 0) {
+		view.scaleBy(0.6);
+	}
+	else {
+		group.allowDrag = true;
+		view.scaleBy(3.4);
+		view.x = 1615;
+		view.y = 2800;
+	}
+}
+
+app.onconnect(viewSetup);
+app.listen(3500);
