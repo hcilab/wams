8ef607d8234064b508a05541febbac61181e6c03
Kolton Gagnon <Kolton.Gagnon@Gmail.com>
2022-09-18 20:54:17 -0400

    chore: add core-js dependency to resolve test

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+module.exports = {
+  modulePaths: ["<rootDir>", "<rootDir>/src"],
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
+};
