88a85e948f0c694537a4fba6942d3b2df47c21f8
Michael van der Kamp <michael.vanderkamp@usask.ca>
2023-04-15 13:16:53 -0700

    Remove wallaby.conf.js

--- wallaby.conf.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (wallaby) {
-  return {
-    files: ['src/**/*.js'],
-    tests: ['test/**/*.js'],
-    env: { type: 'node', runner: 'node' },
-    testFramework: 'jest',
-  };
-};
