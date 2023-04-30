951321065442e8a014b7742d252bd2d084cca0b6
Michael van der Kamp <michael.vanderkamp@usask.ca>
2023-04-30 12:47:11 -0700

    Replace Transform with a change coalescing approach
     - Westures has smart handling for keyboard presses, better to avoid
       replicating it

--- src/client/Interactor.js
+++ src/client/Interactor.js
@@ -4,7 +4,6 @@
 const Westures = require('westures');
 
 const { NOP } = require('../shared.js');
-const Transform = require('./Transform.js');
 
 /**
  * The Interactor class provides a layer of abstraction between the
@@ -41,6 +40,14 @@ class Interactor {
      */
     this.handlers = { ...Interactor.DEFAULT_HANDLERS, ...handlers };
 
+    /**
+     * Object to coalesce state from multiple gestures during one event cycle.
+     *
+     * @type {Object}
+     */
+    this._changes = this._resetChanges()
+    this._scheduled = false;
+
     // Begin listening activities immediately.
     this.bindRegions(root);
     window.addEventListener('wheel', this.wheel.bind(this), false);
@@ -52,18 +59,63 @@ class Interactor {
    * takes care of those activities.
    */
   bindRegions(root) {
+    const pan = new Westures.Pan(root, this.coalesce.bind(this), { disableKeys: ['ctrlKey'] });
+    const pinch = new Westures.Pinch(root, this.coalesce.bind(this));
+    const rotate = new Westures.Rotate(root, this.coalesce.bind(this));
     const swipe = new Westures.Swipe(root, this.handlers.swipe);
     const swivel = new Westures.Swivel(root, this.swivel.bind(this), { enableKeys: ['ctrlKey'] });
     const tap = new Westures.Tap(root, this.handlers.tap);
     const track = new Westures.Track(root, this.handlers.track, { phases: ['start', 'end'] });
-    const transform = new Transform(root, this.handlers.transform);
 
     const region = new Westures.Region(root);
+    region.addGesture(pan);
+    region.addGesture(pinch);
+    region.addGesture(rotate);
     region.addGesture(tap);
     region.addGesture(swipe);
     region.addGesture(swivel);
-    region.addGesture(transform);
     region.addGesture(track);
+  }
+
+  _resetChanges() {
+    return {
+      centroid: { x: 0, y: 0 },
+      delta: {
+        scale: 1,
+        rotation: 0,
+        translation: { x: 0, y: 0 },
+      },
+    }
+  }
+
+  /**
+   * Coalesce state changes for this event cycle.
+   */
+  coalesce(data) {
+    this._changes.centroid = data.centroid;
+    if (data.scale) {
+      this._changes.delta.scale *= data.scale;
+    }
+    if (data.rotation) {
+      this._changes.delta.rotation += data.rotation;
+    }
+    if (data.translation) {
+      this._changes.delta.translation.x += data.translation.x;
+      this._changes.delta.translation.y += data.translation.y;
+    }
+    if (!this._scheduled) {
+      window.setTimeout(this._emit.bind(this), 0);
+      this._scheduled = true;
+    }
+  }
+
+  /**
+   * Emit a transform event with the coalesced state changes.
+   */
+  _emit() {
+    this.handlers.transform(this._changes);
+    this._changes = this._resetChanges()
+    this._scheduled = false;
   }
 
   /**
