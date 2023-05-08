6ff9a657b1ce0690a4e4bda3843349733bc52a8b
Michael van der Kamp <michael.vanderkamp@usask.ca>
2023-05-07 19:49:00 -0700

    Simplify Message to just an object

--- src/shared/Message.js
+++ src/shared/Message.js
@@ -1,32 +1,13 @@
-/*
- * Shared Message class for the WAMS application.
- *
- * Author: Michael van der Kamp
- * Date: July / August 2018
- *
- * The purpose of this class is to provide a funnel through which all messages
- * between client and server must pass. In concert with the Reporter interface,
- * it allows for a sanity check such that the correct sort of data is getting
- * passed back and forth.
- *
- * Unfortunately this does not provide a strict guarantee that informal and ad
- * hoc messages aren't getting emitted somewhere, so it is up to the programmer
- * to be disciplined and adhere to the Message / Reporter principle.
- */
-
 'use strict';
 
-const { defineOwnImmutableEnumerableProperty } = require('./utilities.js');
-
 /**
- * TYPES is an explicit list of the types of messages that will be passed back
- * and forth. Messages not on this list should be ignored!
+ * Enum of the various message types.
  *
+ * @name Message
  * @enum {string}
  * @readonly
- * @lends module:shared.Message
  */
-const TYPES = {
+const Message = {
   // For the server to inform about changes to the model
   /** @const */ ADD_ELEMENT: 'add-element',
   /** @const */ ADD_IMAGE: 'add-image',
@@ -69,66 +50,6 @@ const TYPES = {
   // User defined actions
   /** @const */ DISPATCH: 'dispatch',
 };
-Object.freeze(TYPES);
-
-const TYPE_VALUES = Object.freeze(Object.values(TYPES));
-
-/**
- * The Message class provides a funnel through which data passed between the
- * client and server must flow.
- *
- * If an invalid type is received, the constructor throws an exception. If an
- * invalid reporter is received, an exception will not be thrown until
- * 'emitWith()' is called.
- *
- * @memberof module:shared
- *
- * @throws TypeError
- *
- * @param {string} type - The message type. Must be one of the explicitly listed
- * message types available on the Message object.
- * @param {object} data - The data to be sent.
- * the data to be emitted.
- */
-class Message {
-  constructor(type, data) {
-    if (!TYPE_VALUES.includes(type)) {
-      throw new TypeError('Invalid message type!');
-    }
-
-    /**
-     * The type of Message. Must be one of the predefined types available as
-     * static fields on the Message class.
-     *
-     * @type {string}
-     */
-    this.type = type;
-
-    /**
-     * The data to be sent.
-     *
-     * @type {object}
-     */
-    this.data = data;
-  }
-
-  /**
-   * Emits the data contained along the channel defined by the emitter.
-   *
-   * @param {Emitter} emitter - An object capable of emitting data packets. Must
-   * have an 'emit()' function.
-   */
-  emitWith(emitter) {
-    emitter.emit(this.type, this.data);
-  }
-}
-
-/*
- * Only define the messages once, above, and now attach them to the Message
- * Class object for external reference.
- */
-Object.entries(TYPES).forEach(([p, v]) => {
-  defineOwnImmutableEnumerableProperty(Message, p, v);
-});
+Object.freeze(Message);
 
 module.exports = Message;
