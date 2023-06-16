a159161353845d231bb0b0721419c241c818acb4
Michael van der Kamp <michael.vanderkamp@usask.ca>
2023-06-16 14:06:59 -0600

    Add predefined routing utilities module

--- src/predefined.js
+++ src/predefined.js
@@ -17,10 +17,12 @@ const items = require('./predefined/items.js');
 const layouts = require('./predefined/layouts.js');
 const utilities = require('./predefined/utilities.js');
 const actions = require('./predefined/actions.js');
+const routing = require('./predefined/routing.js');
 
 module.exports = Object.freeze({
   actions,
   items,
   layouts,
   utilities,
+  routing,
 });
---
+++ src/predefined/routing.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// External modules
+const os = require('os');
+
+/**
+ * Server routing utilities.
+ *
+ * @namespace routing
+ * @memberof module:predefined
+ */
+
+/**
+ * @memberof module:predefined.routing
+ *
+ * @returns {string} The first valid local non-internal IPv4 address it finds.
+ */
+function getLocalIP() {
+  for (const networkInterface of Object.values(os.networkInterfaces())) {
+    for (const networkAddress of networkInterface) {
+      if (networkAddress.family === 'IPv4' && networkAddress.internal === false) {
+        return networkAddress.address;
+      }
+    }
+  }
+  return null;
+}
+
