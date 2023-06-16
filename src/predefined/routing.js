6cc544303f777841551287feca92a9cd2a5e4729
Michael van der Kamp <michael.vanderkamp@usask.ca>
2023-06-16 14:27:57 -0600

    Add Router to predefined.routing module with only staticfiles

--- src/predefined/routing.js
+++ src/predefined/routing.js
@@ -26,6 +26,24 @@ function getLocalIP() {
   return null;
 }
 
+/**
+ * This function wraps a layer of abstraction around the route handler. It will
+ * set up the main routes for a WAMS app before returning, so that those routes
+ * will get priority.
+ *
+ * @memberof module:server
+ *
+ * @return {express.app} An express app with main WAMS routes established.
+ */
+function Router() {
+  // Establish router for predefined staticfiles
+  const app = express();
+  app.use('/', express.static(path.join(__dirname, 'staticfiles')));
+  app.use('/', express.static(path.join(__dirname, '..', '..', 'dist', 'wams')));
+  return app;
+}
+
 module.exports = {
   getLocalIP,
+  Router,
 };
