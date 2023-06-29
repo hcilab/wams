f10ce655057205b8248091c4f6010d64edac1b22
Michael van der Kamp <mvanderkamp@coffeemeetsbagel.com>
2023-06-29 08:32:42 -0700

    Run prettier over new examples

--- examples/client/custom-events.js
+++ examples/client/custom-events.js
@@ -2,33 +2,33 @@ const canvas = document.querySelector('#wams');
 const myID = Math.floor(Math.random() * 10000);
 
 canvas.addEventListener('pointerdown', (event) => {
-    const eventDescription = { x: event.x, y: event.y };
-    //console.log(WAMS);
-    WAMS.dispatch('mousedown', eventDescription);
+  const eventDescription = { x: event.x, y: event.y };
+  //console.log(WAMS);
+  WAMS.dispatch('mousedown', eventDescription);
 });
 
 canvas.addEventListener('click', (event) => {
-    const eventDescription = { x: event.x, y: event.y };
-    //console.log(eventDescription);
-    WAMS.dispatch('mouseup', eventDescription);
+  const eventDescription = { x: event.x, y: event.y };
+  //console.log(eventDescription);
+  WAMS.dispatch('mouseup', eventDescription);
 });
 
 canvas.addEventListener('pointerup', (event) => {
-    const eventDescription = { x: event.x, y: event.y };
-    //console.log(eventDescription);
-    WAMS.dispatch('mouseup', eventDescription);
+  const eventDescription = { x: event.x, y: event.y };
+  //console.log(eventDescription);
+  WAMS.dispatch('mouseup', eventDescription);
 });
 
 canvas.addEventListener('pointermove', (event) => {
-    const eventDescription = { x: event.x, y: event.y, id: myID };
-    //console.log(eventDescription);
-    WAMS.dispatch('mousemove', eventDescription);
+  const eventDescription = { x: event.x, y: event.y, id: myID };
+  //console.log(eventDescription);
+  WAMS.dispatch('mousemove', eventDescription);
 });
 
 document.addEventListener('keydown', (event) => {
-    const eventDescription = { key: event.key };
-    //console.log(eventDescription);
-    WAMS.dispatch('keydown', eventDescription);
+  const eventDescription = { key: event.key };
+  //console.log(eventDescription);
+  WAMS.dispatch('keydown', eventDescription);
 });
 
 // canvas.addEventListener("keypressed", (event) => {
@@ -43,17 +43,17 @@ let elapsedList = [];
 let total = 0;
 
 function handleMyOtherMessage(data) {
-    //console.log(data.detail.msg);
-    if (data.detail.time) {
-        let elapsed = Date.now() - data.detail.time;
-        elapsedList.push(elapsed);
-    }
+  //console.log(data.detail.msg);
+  if (data.detail.time) {
+    let elapsed = Date.now() - data.detail.time;
+    elapsedList.push(elapsed);
+  }
 
-    if (data.detail.msg == 1000) {
-        for (let i = 0; i < 1000; i++) {
-            total += elapsedList[i];
-            console.log(total);
-        }
-        console.log('Mean RTT: ', total / 1000);
+  if (data.detail.msg == 1000) {
+    for (let i = 0; i < 1000; i++) {
+      total += elapsedList[i];
+      console.log(total);
     }
+    console.log('Mean RTT: ', total / 1000);
+  }
 }
